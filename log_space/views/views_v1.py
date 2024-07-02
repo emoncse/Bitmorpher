3fb339b44fa68354d9627c8c25b5cd4f03fce491
IMRAN NAZIR EMON <emon.genius@gmail.com>
2024-07-03 00:57:08 +0600

    [update] Bitmorpher: [BTM004] BTM004
    1. Pagination added for the large amount of response. The request can also handle page size and page number using query params.
    2. Logger added for each function.
    3. Core setting has some minor changes.
    
    BTM004
    
    On branch development
    Changes to be committed:
          modified:   core/__pycache__/settings.cpython-39.pyc
          modified:   core/__pycache__/urls.cpython-39.pyc
          modified:   core/settings.py
          modified:   core/urls.py
          new file:   external/__pycache__/paginated_response.cpython-39.pyc
          new file:   external/paginated_response.py
          deleted:    log_space/views.py
          modified:   log_space/views/__pycache__/views_v1.cpython-39.pyc
          modified:   log_space/views/views_v1.py
          new file:   logs/request.log
          modified:   user_panel/views/__pycache__/views_v1.cpython-39.pyc
          modified:   user_panel/views/views_v1.py

--- log_space/views/views_v1.py
+++ log_space/views/views_v1.py
@@ -1,10 +1,16 @@
+import logging
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
+from external.paginated_response import CustomPagination
 from log_space.models import APILog
 from log_space.serializers.serializers_v1 import LogSpaceSerializer
 from user_panel.permissions.permission_v1 import IsManager
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 @extend_schema(tags=['log_space'])
@@ -13,6 +19,7 @@ class LogSpaceView(viewsets.ReadOnlyModelViewSet):
     queryset = APILog.objects.all()
     serializer_class = LogSpaceSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = CustomPagination
 
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
@@ -20,5 +27,17 @@ class LogSpaceView(viewsets.ReadOnlyModelViewSet):
         return [IsAuthenticated(), IsManager()]
 
     def get_queryset(self):
+        logger.info('Fetching APILog queryset.')
         return self.queryset
 
+    def list(self, request, *args, **kwargs):
+        logger.info('Listing API logs.')
+        response = super().list(request, *args, **kwargs)
+        logger.info('Listed API logs successfully.')
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        logger.info(f'Retrieving API log with id {kwargs.get("pk")}.')
+        response = super().retrieve(request, *args, **kwargs)
+        logger.info(f'API log with id {kwargs.get("pk")} retrieved successfully.')
+        return response
